b6612c749da5c0556707879f5a82e31f8b457830
ramy-salama <family.pro.000@gmail.com>
2021-04-30 16:57:25 +0300

    modified:   index.html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
 var myicon = document.getElementById("click"),
-            myside = document.getElementById("sidebar");
+    myside = document.getElementById("sidebar");
 
-        myicon.onclick = function () {
-            if (myside.classList.contains("hide")) {
-                myside.classList = "show";
-            } else {
-                myside.classList = "hide";
-            }
-        }
+myicon.onclick = function () {
+    if (myside.classList.contains("hide")) {
+        myside.classList = "show";
+    } else {
+        myside.classList = "hide";
+    }
+}
+
+var sliderTimeoutID;
+var element = 1;
+
+function delayedSlider() {
+    element == 5 ? element = 1 : element = element;
+    element == 1 ? pElement = 4 : pElement = element - 1;
+    document.getElementById("news" + pElement).style.display = "none";
+    document.getElementById("news" + element).style.display = "block";
+    document.getElementById("newsLink" + pElement).className = "newsLinks";
+    document.getElementById("newsLink" + element).className = "newsLinks selected";
+    sliderTimeoutID = window.setTimeout(delayedSlider, 6000);
+    element++;
+}
+
+function showSlide(num) {
+    window.clearTimeout(sliderTimeoutID);
+    document.getElementById("news1").style.display = "none";
+    document.getElementById("news2").style.display = "none";
+    document.getElementById("news3").style.display = "none";
+    document.getElementById("news4").style.display = "none";
+    document.getElementById("news5").style.display = "none";
+    document.getElementById("newsLink1").className = "newsLinks";
+    document.getElementById("newsLink2").className = "newsLinks";
+    document.getElementById("newsLink3").className = "newsLinks";
+    document.getElementById("newsLink4").className = "newsLinks";
+    document.getElementById("newsLink5").className = "newsLinks";
+    document.getElementById("news" + num).style.display = "block";
+    document.getElementById("newsLink" + num).className = "newsLinks selected";
+    element = num + 1;
+    sliderTimeoutID = window.setTimeout(delayedSlider, 6000);
+}
+delayedSlider();
